042854ff39babc07db9dc881020f41abf276f3c9
Данила Золотов <zolotov.dk@students.dvfu.ru>
2023-01-14 02:03:29 +1000

    a

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,11 +7,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
 import Game from "./pages/Game/Game.vue";
 import Main from "./pages/Main/Main.vue";
-import LoginForm from "./components/Forms/LoginForm/LoginForm.vue";
+
 
 const routes = [
 	{ path: "/game", component: Game },
-	{ path: "/login", component: LoginForm },
 	{ path: "/", component: Main },
 ];
 
--- frontend/src/pages/Main/Main.vue
+++ frontend/src/pages/Main/Main.vue
@@ -5,23 +5,37 @@
 
 	import Modal from "../../components/Modal/Modal.vue";
 	import Button from "../../components/UI/Button/Button.vue";
-	import Link from "../../components/UI/Link/Link.vue";
 	import css from "./Main.module.css";
-
+	import LoginForm from "../../components/Forms/LoginForm/LoginForm.vue";
+	import Stats from "../../components/Profile/ProfileStats/ProfileStats.vue";
 	const isNewGameOpen = ref(false);
 	const isConnectOpen = ref(false);
+	const isRegisterOpen = ref(false);
+	const isLoginOpen = ref(false);
 
 	const openNewGameTrigger = () => (isNewGameOpen.value = !isNewGameOpen.value);
 	const openConnectTrigger = () => (isConnectOpen.value = !isConnectOpen.value);
+	const openRegisterTrigger = () => (isRegisterOpen.value = !isRegisterOpen.value);
+	const openLoginTrigger = () => (isLoginOpen.value = !isLoginOpen.value);
 </script>
 
 <template>
 	<div :class="css.main__wrapper">
+		<div :class="css.profile">
+			<Stats />
+		</div>
+		<img src ="logo.png" :class ="css.logo">
+		<div :class="css.logo__text">TicTacToe</div>
 		<main :class="css.main">
-			<Link to="/login">Вход</Link>
+			<div :class="css.main__auth">
+				<Button  @click="openLoginTrigger">Вход</Button>
+				<Button @click="openRegisterTrigger">Регистрация</Button>
+				
+			</div>
 			<Button @click="openNewGameTrigger">Создать новую игру</Button>
 			<Button @click="openConnectTrigger">Присоединиться</Button>
-
+			<Modal :isOpen="isRegisterOpen" @close="openRegisterTrigger"><LoginForm text = "Регистрация"/></Modal>
+			<Modal :isOpen="isLoginOpen" @close="openLoginTrigger"><LoginForm text = "Логин"/></Modal>
 			<Modal :isOpen="isNewGameOpen" @close="openNewGameTrigger"><CreateGameForm /></Modal>
 			<Modal :isOpen="isConnectOpen" @close="openConnectTrigger"><ConnectGameForm /></Modal>
 		</main>
